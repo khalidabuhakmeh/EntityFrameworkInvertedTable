fa7383f52e3e9dc067d88542d4cc6bc63845cc89
Khalid Abuhakmeh <khalidabuhakmeh@gmail.com>
2022-04-21 13:11:10 -0400

    initial commit

---
+++ Migrations/20220421165426_initial migration.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EntityFrameworkInvertedTable.Migrations
+{
+    public partial class initialmigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "EntityWithInvertedTables",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    CreatedAt = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EntityWithInvertedTables", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "EntityWithJsons",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    CreatedAt = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Values = table.Column<string>(type: "BLOB", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EntityWithJsons", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CustomValues",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    EntityWithInvertedTableId = table.Column<int>(type: "INTEGER", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    Value = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CustomValues", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_CustomValues_EntityWithInvertedTables_EntityWithInvertedTableId",
+                        column: x => x.EntityWithInvertedTableId,
+                        principalTable: "EntityWithInvertedTables",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CustomValues_EntityWithInvertedTableId",
+                table: "CustomValues",
+                column: "EntityWithInvertedTableId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CustomValues");
+
+            migrationBuilder.DropTable(
+                name: "EntityWithJsons");
+
+            migrationBuilder.DropTable(
+                name: "EntityWithInvertedTables");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,113 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using System.Text.Json;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+var storing = new Database();
+
+storing.Database.Migrate();
+
+var json = new EntityWithJson
+{
+    Values = new()
+    {
+        {"Name", "Khalid"},
+        {"Status", "Awesome"}
+    }
+};
+
+var invertedTable = new EntityWithInvertedTable()
+    .AddValue("Name", "Khalid")
+    .AddValue("Status", "Awesome... Again!");
+
+storing.EntityWithJsons.Add(json);
+storing.EntityWithInvertedTables.Add(invertedTable);
+storing.SaveChangesAsync();
+
+var reading = new Database();
+
+json = reading.EntityWithJsons.OrderByDescending(o => o.CreatedAt).First();
+
+invertedTable = reading.EntityWithInvertedTables
+    .OrderByDescending(o => o.CreatedAt)
+    // include values from other table
+    .Include(x => x.Values)
+    .First();
+
+Console.WriteLine("Results from JSON are...");
+foreach (var (key, value) in json.Values) 
+{
+    Console.WriteLine($"  - {key}: {value}");
+}
+
+Console.WriteLine("\nResults from Entity are...");
+foreach (var entry in invertedTable.Values) 
+{
+    Console.WriteLine($"  - {entry.Name}: {entry.Value}");
+}
+
+public class Database : DbContext
+{
+    public DbSet<EntityWithJson> EntityWithJsons { get; set; }
+    public DbSet<EntityWithInvertedTable> EntityWithInvertedTables { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlite("Data Source=database.db");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        var options = new JsonSerializerOptions(JsonSerializerDefaults.General);
+
+        modelBuilder
+            .Entity<EntityWithJson>()
+            .Property(x => x.Values)
+            .HasColumnName("Values")
+            .HasColumnType("BLOB") // sqlite BLOB type
+            .HasConversion(
+                v => JsonSerializer.Serialize(v, options),
+                s => JsonSerializer.Deserialize<Dictionary<string, string>>(s, options)!,
+                ValueComparer.CreateDefault(typeof(Dictionary<string, string>), true)
+            );
+    }
+}
+
+public class EntityWithJson
+{
+    public int Id { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public Dictionary<string, string> Values { get; set; }
+        = new();
+}
+
+public class EntityWithInvertedTable
+{
+    public int Id { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public ICollection<CustomValues> Values { get; set; }
+        = new List<CustomValues>();
+
+    public EntityWithInvertedTable AddValue(string name, string value)
+    {
+        var existing = Values?.FirstOrDefault(v => v.Name.Equals(name, StringComparison.OrdinalIgnoreCase));
+        if (existing is { })
+        {
+            existing.Value = value;
+        }
+        else
+        {
+            Values?.Add(new CustomValues {Name = name, Value = value});
+        }
+
+        return this;
+    }
+}
+
+public class CustomValues
+{
+    public int Id { get; set; }
+    public int EntityWithInvertedTableId { get; set; }
+    public string Name { get; set; }
+    public string Value { get; set; }
+}
